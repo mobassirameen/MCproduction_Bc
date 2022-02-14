f61a3292292eb64738788413cad574a38180114d
mobassirameen <mobassir.ameen7866@gmail.com>
2022-02-14 13:12:19 +0100

    updated

--- MCcrabJobScript.sh
+++ MCcrabJobScript.sh
@@ -4,12 +4,12 @@ CHANNEL_DECAY="BcToBsPi"
 YEAR="2018"
 
 # Change the release name of each step  accordingly
-GEN_REL="CMSSW_8_0_31"
-GEN_SCRAM="slc7_amd64_gcc530"
-RECO_REL="CMSSW_8_0_31"
-RECO_SCRAM="slc7_amd64_gcc530"
-MINI_REL="CMSSW_8_0_31"
-MINI_SCRAM="slc7_amd64_gcc530"
+GEN_REL="CMSSW_10_6_18"
+GEN_SCRAM="slc7_amd64_gcc700"
+RECO_REL="CMSSW_10_6_18"
+RECO_SCRAM="slc7_amd64_gcc700"
+MINI_REL="CMSSW_10_6_18"
+MINI_SCRAM="slc7_amd64_gcc700"
 
 echo "================= cmssw environment prepration Gen step ===================="
 source /cvmfs/cms.cern.ch/cmsset_default.sh
@@ -24,10 +24,10 @@ eval `scram runtime -sh`
 scram b
 cd ../../
 
-echo "================= PB: CMSRUN starting Gen step ===================="
+echo "================= PB: cmsRun starting Gen step ===================="
 # PSet is the name that crab assigns to the config file of the job
-cmsRun -j ${CHANNEL_DECAY}_step0.log  -p PSet.py
-#cmsRun -j ${CHANNEL_DECAY}_step0.log -p step0-GS_BuJpsiK_Pythia8_8p16TeV_TuneCUETP8M1_GEN.py
+#cmsRun -j ${CHANNEL_DECAY}_step0.log  -p PSet.py
+cmsRun -j ${CHANNEL_DECAY}_step0.log -p step0-GS_BcToBsPi_Pythia8_13TeV_TuneCP5_GEN.py
 
 
 echo "================= cmssw environment prepration Reco step ===================="
@@ -42,14 +42,14 @@ eval `scram runtime -sh`
 scram b
 cd ../../
 
-echo "================= PB: CMSRUN starting Reco step 1 ===================="
-cmsRun -e -j ${CHANNEL_DECAY}_step1.log step1-DR_BuJpsiK_Pythia8_8p16TeV_TuneCUETP8M1_DIGI.py
+echo "================= PB: CMSRUN starting Digi step 1 ===================="
+cmsRun -e -j ${CHANNEL_DECAY}_step1.log step1-DR_BcToBsPi_Pythia8_13TeV_TuneCP5_DIGI.py
 #cleaning
 #rm -rfv step0-GS_BuJpsiK_Pythia8_8p16TeV_TuneCUETP8M1_GEN.root
 
 echo "================= PB: CMSRUN starting Reco step 2 ===================="
-#cmsRun -e -j ${CHANNEL_DECAY}_step2.log  step2-DR_BuJpsiK_Pythia8_8p16TeV_TuneCUETP8M1_RECO.py
-cmsRun -e -j FrameworkJobReport.xml  step2-DR_BuJpsiK_Pythia8_8p16TeV_TuneCUETP8M1_RECO.py
+cmsRun -e -j ${CHANNEL_DECAY}_step2.log step2-DR_BcToBsPi_Pythia8_13TeV_TuneCP5_RECO.py
+#cmsRun -e -j FrameworkJobReport.xml  step2-DR_BuJpsiK_Pythia8_8p16TeV_TuneCUETP8M1_RECO.py
 #cleaning
 #rm -rfv step1-DR_BuJpsiK_Pythia8_8p16TeV_TuneCUETP8M1_DIGI.root
 
